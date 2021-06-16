572cdedc61904d6f217d2d286709348069a024de
michischmidt <schmidt.m.dev@gmail.com>
2021-06-16 19:26:29 +0200

    added log for accels

--- analyze.py
+++ analyze.py
@@ -3,9 +3,9 @@
 # -*- coding: utf-8 -*-
 # Script was written by Michael Schmidt
 
-'''
+"""
 Start script with: analyze.py <PORT>
-'''
+"""
 
 import sys
 from PyQt5 import QtGui, QtCore
@@ -24,20 +24,41 @@ class NormalVectorNode(Node):
         terminals = {
             "axis1": dict(io="in"),
             "axis2": dict(io="in"),
-            "output": dict(io="out"),
+            "rotation": dict(io="out"),
         }
 
         Node.__init__(self, name, terminals=terminals)
 
     def process(self, **kwds):
-        normal_vector = np.array([])
+        # negativ x axis for mirroring the vector for user perspective
         normal_vector = np.array(
-            [[0, 0], [kwds["axis1"][0], kwds["axis2"][0]]])
-        print(normal_vector)
-        return {"output": normal_vector}
+            [[0, 0], [-kwds["axis1"][0], kwds["axis2"][0]]])
+        return {"rotation": normal_vector}
 
 
-def init_normal_vector_plotting(layout, fc, dippid_node):
+class LogNode(Node):
+
+    nodeName = "LogNode"
+
+    def __init__(self, name):
+        terminals = {
+            "accelX": dict(io="in"),
+            "accelY": dict(io="in"),
+            "accelZ": dict(io="in"),
+            "rotation": dict(io="in"),
+        }
+        Node.__init__(self, name, terminals=terminals)
+
+    def process(self, **kwds):
+        x = kwds["accelX"][0]
+        y = kwds["accelY"][0]
+        z = kwds["accelZ"][0]
+        # rotation = NormalVectorNode.process().rotation
+        # print(rotation)
+        print(f"Acceleration X: {x}, Acceleration Y: {y}, Acceleration Z: {z}")
+
+
+def init_normal_vector_plotting(layout, fc, dippid_node, log_node):
     # create normal vector plot widget
     plot_widget = pg.PlotWidget()
     layout.addWidget(plot_widget, 1, 1)
@@ -57,10 +78,10 @@ def init_normal_vector_plotting(layout, fc, dippid_node):
     fc.connectTerminals(dippid_node["accelY"],
                         plot_normal_vector_node["axis2"])
     fc.connectTerminals(
-        plot_normal_vector_node["output"], plot_widget_node["In"])
+        plot_normal_vector_node["rotation"], plot_widget_node["In"])
 
 
-def init_accel_plotting(layout, fc, dippid_node):
+def init_accel_plotting(layout, fc, dippid_node, log_node):
     inputs = ("accelX", "accelY", "accelZ")
 
     # create plot widgets
@@ -75,24 +96,25 @@ def init_accel_plotting(layout, fc, dippid_node):
     # set plot widget nodes
     plot_widgets_nodes = []
     for i, plot_widget in enumerate(plot_widgets):
-        plot_widget_node = fc.createNode('PlotWidget', pos=(150, i * 100))
+        plot_widget_node = fc.createNode("PlotWidget", pos=(150, i * 100))
         plot_widget_node.setPlot(plot_widget)
         plot_widgets_nodes.append(plot_widget_node)
 
     # create buffer nodes
     buffer_nodes = []
     for i, plot_widget_node in enumerate(plot_widgets_nodes):
-        buffer_node = fc.createNode('Buffer', pos=(300, i * 100))
+        buffer_node = fc.createNode("Buffer", pos=(300, i * 100))
         buffer_nodes.append(buffer_node)
 
-    # connect dippid
+    # connect dippid and log nodes
     for i, input in enumerate(inputs):
-        fc.connectTerminals(dippid_node[input], buffer_nodes[i]['dataIn'])
+        fc.connectTerminals(dippid_node[input], buffer_nodes[i]["dataIn"])
+        fc.connectTerminals(dippid_node[input], log_node[input])
 
     # connect buffer
     for i, buffer_node in enumerate(buffer_nodes):
         fc.connectTerminals(
-            buffer_node['dataOut'], plot_widgets_nodes[i]['In'])
+            buffer_node["dataOut"], plot_widgets_nodes[i]["In"])
 
 
 def get_port():
@@ -103,12 +125,13 @@ def get_port():
     return sys.argv[1]
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     fclib.registerNodeType(NormalVectorNode, [("Data",)])
+    fclib.registerNodeType(LogNode, [("Data",)])
     port = get_port()
     app = QtGui.QApplication([])
     win = QtGui.QMainWindow()
-    win.setWindowTitle('DIPPID Graph')
+    win.setWindowTitle("DIPPID Graph")
 
     # Define a top-level widget to hold everything
     central_widget = QtGui.QWidget()
@@ -125,9 +148,12 @@ if __name__ == '__main__':
     # create DIPPID node
     dippid_node = fc.createNode("DIPPID", pos=(0, 0))
 
-    init_accel_plotting(layout, fc, dippid_node)
-    init_normal_vector_plotting(layout, fc, dippid_node)
+    # create log node
+    log_node = fc.createNode("LogNode", pos=(600, 100))
+
+    init_accel_plotting(layout, fc, dippid_node, log_node)
+    init_normal_vector_plotting(layout, fc, dippid_node, log_node)
 
     win.show()
-    if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
+    if (sys.flags.interactive != 1) or not hasattr(QtCore, "PYQT_VERSION"):
         sys.exit(app.exec_())
