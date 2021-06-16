879ee3ff8031b3626dca8b38c89d85cdc35cbf34
michischmidt <schmidt.m.dev@gmail.com>
2021-06-16 17:07:20 +0200

    added normalvector, still bugged

--- analyze.py
+++ analyze.py
@@ -13,13 +13,54 @@ import pyqtgraph as pg
 from pyqtgraph.flowchart import Flowchart, Node
 import numpy as np
 from DIPPID_pyqtnode import DIPPIDNode, BufferNode
+import pyqtgraph.flowchart.library as fclib
 
 
-def init_plotting(layout, fc):
+class NormalVectorNode(Node):
+
+    nodeName = "NormalVectorNode"
+
+    def __init__(self, name):
+        terminals = {
+            "axis1": dict(io="in"),
+            "axis2": dict(io="in"),
+            "output": dict(io="out"),
+        }
+
+        Node.__init__(self, name, terminals=terminals)
+
+    def process(self, **kwds):
+        print(kwds["axis1"][0])
+        print(kwds["axis2"][0])
+        normal_vector = np.array([kwds["axis1"][0], kwds["axis2"][0]])
+        return {"output": normal_vector}
+
+
+fclib.registerNodeType(NormalVectorNode, [("Data",)])
+
+
+def init_normal_vector_plotting(layout, fc, dippid_node):
+    # create plot widgets
+    plot_widget = pg.PlotWidget()
+    layout.addWidget(plot_widget, 1, 1)
+    plot_widget.setTitle("Normal Vector")
+    plot_widget.setYRange(-1, 1)
+    plot_widget.setXRange(0, 1)
+
+    # set plot widget nodes
+    plot_widget_node = fc.createNode("PlotWidget", pos=(450, 100))
+    plot_normal_vector_node = fc.createNode(
+        "NormalVectorNode", pos=(450, 200))
+    plot_widget_node.setPlot(plot_normal_vector_node)
+
+    # connect dippid
+    fc.connectTerminals(dippid_node["accelX"], plot_normal_vector_node["axis1"])
+    fc.connectTerminals(dippid_node["accelY"], plot_normal_vector_node["axis2"])
+    fc.connectTerminals(plot_normal_vector_node["output"], plot_widget_node["In"])
+
+
+def init_accel_plotting(layout, fc, dippid_node):
     inputs = ("accelX", "accelY", "accelZ")
-
-    # create DIPPID node
-    dippid_node = fc.createNode("DIPPID", pos=(0, -100))
 
     # create plot widgets
     plot_widgets = []
@@ -45,8 +86,6 @@ def init_plotting(layout, fc):
 
     # connect dippid
     for i, input in enumerate(inputs):
-        # print(input)
-        # print(buffer_nodes[i])
         fc.connectTerminals(dippid_node[input], buffer_nodes[i]['dataIn'])
 
     # connect buffer
@@ -78,10 +117,14 @@ if __name__ == '__main__':
     central_widget.setLayout(layout)
 
     # Creating flowchart
-    fc = Flowchart(terminals={'out': dict(io='out')})
+    fc = Flowchart(terminals={})
     layout.addWidget(fc.widget(), 0, 0, 2, 1)
 
-    init_plotting(layout, fc)
+    # create DIPPID node
+    dippid_node = fc.createNode("DIPPID", pos=(0, 0))
+
+    init_accel_plotting(layout, fc, dippid_node)
+    init_normal_vector_plotting(layout, fc, dippid_node)
 
     win.show()
     if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
