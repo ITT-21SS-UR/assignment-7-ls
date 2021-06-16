ca53e3c1a12bb1e560f15ed43de513d11ae1701f
michischmidt <schmidt.m.dev@gmail.com>
2021-06-16 14:00:51 +0200

    added first plots

--- analyze.py
+++ analyze.py
@@ -10,6 +10,23 @@ Start script with: analyze.py <PORT>
 import sys
 from PyQt5 import QtGui, QtCore
 import pyqtgraph as pg
+from pyqtgraph.flowchart import Flowchart, Node
+import numpy as np
+
+
+def plot_accels(layout):
+    fc = Flowchart(terminals={})
+    layout.addWidget(fc.widget(), 0, 0, 2, 1)
+
+    inputs = ("acceleration_x", "acceleration_y", "acceleration_z")
+
+    for i, input in enumerate(inputs):
+        plot_widget = pg.PlotWidget()
+        plot_widget.setTitle(input.title())
+        plot_widget.setYRange(-1, 1)
+        layout.addWidget(plot_widget, 0, i + 1)
+        plot_widget_node = fc.createNode('PlotWidget', pos=(300, i * 100))
+        plot_widget_node.setPlot(plot_widget)
 
 
 def get_port():
@@ -27,12 +44,14 @@ if __name__ == '__main__':
     win.setWindowTitle('DIPPID Graph')
 
     # Define a top-level widget to hold everything
-    cw = QtGui.QWidget()
-    win.setCentralWidget(cw)
+    central_widget = QtGui.QWidget()
+    win.setCentralWidget(central_widget)
 
     #  Create a grid layout to manage the widgets size and position
     layout = QtGui.QGridLayout()
-    cw.setLayout(layout)
+    central_widget.setLayout(layout)
+
+    plot_accels(layout)
 
     win.show()
     if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
